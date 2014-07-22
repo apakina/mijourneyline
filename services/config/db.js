4532bbb33a8de2f3a2909e072681943e23111bea
apakina <apakina@BANL1191b97dd.local>
2014-07-22 23:40:11 +0530

    update config db name

--- services/config/db.js
+++ services/config/db.js
@@ -1,3 +1,3 @@
 // more routes for our API will happen here
 var mongoose   = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/mijourneylinedb1'); // connect to our database
+mongoose.connect('mongodb://localhost:27017/mijourneylinedb'); // connect to our database
